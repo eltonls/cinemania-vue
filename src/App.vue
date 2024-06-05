6173e3cd4e96b573070a74185239a022fdd5752e
Elton <eltonrs1999@gmail.com>
2024-06-05 09:04:24 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="count++">Bora, clica aí {{ count }}</button>
+</template>
+
+<style scoped>
+  button {
+    font-weight: bold;
+    border-radius: 2em;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "./styles.css";
+import app from "./App.vue";
+
+createApp(app).mount("#app");
